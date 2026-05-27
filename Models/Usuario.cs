599260abae2fa7a3ca1cf4b7b6a0a63f41e1e6d2
Antonio L Rivieri Jr <junior@menezessgarbi.com.br>
2026-05-27 17:17:48 -0300

    incl. data e ip ultimo acesso usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -27,6 +27,11 @@ namespace Task4U.Models
         [StringLength(50, MinimumLength = 6, ErrorMessage = "A senha deve ter no mínimo 6 e no máximo 50 caracteres")]
         public string Senha { get; set; } = string.Empty;
 
+        public DateTime UltimoAcessoData { get; set;  } = DateTime.Now;
+
+        [StringLength(15)]
+        public String UltimoAcessoIP { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "O nível de acesso é obrigatório")]
         [Display(Name = "Nível de Acesso")]
         public NivelUsuario Nivel { get; set; } = NivelUsuario.Colaborador;
