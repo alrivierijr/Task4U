d15b37ab4982602b186d6add22f0d0451f2abe2d
Antonio L Rivieri Jr <junior@menezessgarbi.com.br>
2026-05-27 14:35:46 -0300

    Initial commit: born of the boy

---
+++ Infrastructure/TskDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Task4U.Infrastructure
+{
+    public class TskDbContext : DbContext
+    {
+        public TskDbContext(DbContextOptions<TskDbContext> options) : base(options) 
+        {
+            
+        }
+
+    }
+}
