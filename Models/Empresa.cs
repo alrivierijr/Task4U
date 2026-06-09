bfdab6c977fc868914c0b12cea0c4c2413a8a3fe
ALRivieriJr <alrivierijr@outlook.com>
2026-06-09 10:09:45 -0300

    Update validation messages and add new properties

--- Models/Empresa.cs
+++ Models/Empresa.cs
@@ -32,15 +32,65 @@ namespace Task4U.Models
         [Column("razao_social")]
         [Display(Name = "Razão Social")]
         [Required(ErrorMessage = "A razão social é obrigatória.") ]
-        [StringLength(60, ErrorMessage = "A razão social não deve passar de 60 caracteres")]
+        [StringLength(60, ErrorMessage = "A razão social não deve ultrapassar de 60 caracteres")]
         public string RazaoSocial { get; set; } = string.Empty;
 
         [Column("nome_fantasia")]
         [Display(Name = "Nome Fantasia")]
         [Required(ErrorMessage = "O nome fantasia é obrigatório.") ]
-        [StringLength(60, ErrorMessage = "O nome fantasia não deve passar de 60 caracteres")]
+        [StringLength(60, ErrorMessage = "O nome fantasia não deve ultrapassar de 60 caracteres")]
         public string NomeFantasia { get; set; } = string.Empty;
 
+        [Column("inscricao_municipal")]
+        [Display(Name = "Inscrição Municipal")]
+        [Required(ErrorMessage = "A Inscrição Municipal é obrigatória.") ]
+        [StringLength(20, ErrorMessage = "A Inscrição Municipal não deve ultrapassar 20 caracteres")]
+        public string InscricaoMunicipal { get; set; } = string.Empty;
+        
+        [Column("inscricao_estadual")]
+        [Display(Name = "Inscrição Estadual")]
+        [Required(ErrorMessage = "A Inscrição Estadual é obrigatória.") ]
+        [StringLength(20, ErrorMessage = "A Inscrição Estadual não deve ultrapassar 20 caracteres")]
+        public string InscricaoEstadual { get; set; } = string.Empty;
+
+        [Column("cnpj")]
+        [Display(Name = "CNPJ")]
+        [Required(ErrorMessage = "O CNPJ é obrigatório.") ]
+        [StringLength(14, ErrorMessage = "O CNPJ não deve ultrapassar 14 caracteres")]
+        public string CNPJ { get; set; } = string.Empty;
+
+        [Column("logradouro")]
+        [Display(Name = "Logradouro")]
+        [Required(ErrorMessage = "O logradouro é obrigatório.") ]
+        [StringLength(60, ErrorMessage = "O logradouro não deve ultrapassar 60 caracteres")]
+        public string Logradouro { get; set; } = string.Empty;
+
+        [Column("numero")]
+        [Display(Name = "Número")]
+        [Required(ErrorMessage = "O número é obrigatório.") ]
+        [StringLength(10, ErrorMessage = "O número não deve ultrapassar 10 caracteres")]
+        public string Número { get; set; } = string.Empty;
+
+        [Column("complemento")]
+        [Display(Name = "Complemento")]
+        [StringLength(20, ErrorMessage = "O complemento não deve ultrapassar 20 caracteres")]
+        public string Complemento { get; set; } = string.Empty;
+
+        [Column("bairro")]
+        [Display(Name = "Bairro")]
+        [StringLength(40, ErrorMessage = "O bairro não deve ultrapassar 40 caracteres")]
+        public string Bairro { get; set; } = string.Empty;
+
+        [Column("cep")]
+        [Display(Name = "CEP")]
+        [StringLength(9, ErrorMessage = "O CEP não deve ultrapassar 9 caracteres")]
+        public string Cep { get; set; } = string.Empty;
+
+        [Column("telefone")]
+        [Display(Name = "Telefone")]
+        [StringLength(10, ErrorMessage = "O telefone não deve ultrapassar 10 caracteres")]
+        public string Telefone { get; set; } = string.Empty;
+        
         [Column("email")]
         [Display(Name = "E-mail")]
         [Required(ErrorMessage = "O e-mail é obrigatório.")]
